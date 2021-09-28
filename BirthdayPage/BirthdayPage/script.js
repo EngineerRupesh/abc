76c7eff3a552946d15cb97842e0157f0b4722000
EngineerRupesh <91221018+EngineerRupesh@users.noreply.github.com>
2021-09-28 21:05:26 +0530

    Add files via upload

---
+++ BirthdayPage/BirthdayPage/script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter UserName");
+ var b=prompt("Enter password");
+  	if(a== "rupesh" && b=="24"||a=="gulshan"&&b=="25" || a=="kushi"&&b=="26")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
